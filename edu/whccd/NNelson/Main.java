304976dd978f7ec2b51ff289f7149b899c7bddaf
NicolayNelson <59945806+NicolayNelson@users.noreply.github.com>
2020-04-02 21:39:22 -0700

    InvoiceApp (BigDecimal)

--- edu/whccd/NNelson/Main.java
+++ edu/whccd/NNelson/Main.java
@@ -1,9 +1,11 @@
 package edu.whccd.nnelson;
 
 import java.util.Scanner;
-
+import java.text.NumberFormat;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 /*
-    The purpose of this application is to replicate the code in Figure 2-17.
+    The purpose of this application is to replicate the code in Figure 3-17.
     author:Nicolay Nelson
  */
 
@@ -11,44 +13,45 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Invoice Total Calculator");
-        System.out.println(); //print a blank line
+        final BigDecimal SALES_TAX_PCT = new BigDecimal (".05");
 
         Scanner sc = new Scanner(System.in);
-
-        // perform invoice calculations until choice isn't equal to "y" or "Y"
         String choice = "y";
         while (choice.equalsIgnoreCase("y")) {
-            // get the invoice subtotal from the user
-            System.out.print("Enter Subtotal:   ");
-            double subtotal = sc.nextDouble();
+            System.out.print("Enter subtotal:   ");
+            String subtotalString = sc.next();
 
-            //get the discount percent
-            double discountPercent;
-            if (subtotal >= 200) {
-                discountPercent = .2;
-            } else if (subtotal >= 100) {
-                discountPercent = .1;
+            // create the BigDecimal objects for subtotal and discount percent
+            BigDecimal subtotal = new BigDecimal (subtotalString);
+            BigDecimal discountPercent;
+            if (subtotal.doubleValue()  >= 100) {
+                discountPercent = new BigDecimal(".1");
             } else {
-                discountPercent = 0.0;
+                discountPercent = new BigDecimal("0.0");
             }
 
-            //calculate the discount amount and total
-            double discountAmount = subtotal * discountPercent;
-            double total = subtotal - discountAmount;
+            // calculate the results
+            BigDecimal discountAmount = subtotal.multiply(discountPercent)
+                    .setScale(2, RoundingMode.HALF_UP);
+            BigDecimal totalBeforeTax = subtotal.subtract(discountAmount);
+            BigDecimal salesTax = SALES_TAX_PCT.multiply(totalBeforeTax)
+                    .setScale(2, RoundingMode.HALF_UP);
+            BigDecimal total = totalBeforeTax.add(salesTax);
 
-            //display the results
-            String message = "Discount percent: " + discountPercent + "\n"
-                    + "Discount amount: " + discountAmount + "\n"
-                    + "Invoice total: " + total + "\n";
+            //format and display the results
+            NumberFormat currency = NumberFormat.getCurrencyInstance();
+            NumberFormat percent = NumberFormat.getPercentInstance();
+            String message =
+                    "Discount percent:  " + percent.format(discountPercent) + "\n"
+                            +  "Discount amount:   " + currency.format(discountAmount) + "\n"
+                            +  "Total before tax:  " + currency.format(totalBeforeTax) + "\n"
+                            +  "Sales Tax:         " + currency.format(salesTax) + "\n"
+                            +  "Invoice total:     " + currency.format(total) + "\n";
             System.out.println(message);
 
-            // see if the user wants to continue
-            System.out.println("Continue? (y/n): ");
+            System.out.print("Continue? (y/n):  ");
             choice = sc.next();
             System.out.println();
         }
     }
 }
-
-
